751d2f789830e2092183992059bcdf4f8ea23f3f
kyliemcneil06-cmd <kyliemcneil06@gmail.com>
2026-06-04 15:05:18 -0400

    not working

--- sketches/remix.js
+++ sketches/remix.js
@@ -18,8 +18,34 @@ function draw() {
 
 // ________________________________
 
+  function draww() {
+  // // Create a black rectangle to cover the canvas.
+  // // Make the rectangle black with an alpha value of 12 so that
+  // // the white circle following the cursor slowly fades into the background.
+  // background(70, 19);
+  // // 12
 
+  // Create the circle that will follow the cursor as it hovers
+  // over the canvas.
+  fill(300);
+  noStroke();
+  ellipse(mouseX, mouseY, 20, 20);
 
+  // Give the buffer a dark grey background.
+  // Any shapes within the buffer will have no fill.
+  // graphic.background(51);
+  // graphic.noFill();
+
+  // When the cursor hovers over the offscreen buffer, replicate the
+  // circle that is drawn when the cursor is hovering over the
+  // canvas. Within the buffer area, only show the outline of the circle.
+  graphic.stroke(250);
+  graphic.ellipse(mouseX - 150, mouseY - 75, 60, 60);
+
+  // Draw the buffer to the screen with image().
+  image(graphic, 150, 75);
+  }
+  
 function draw() {
   background(0);
 
@@ -43,6 +69,7 @@ function draw() {
   describe(
     'A tree drawn by recursively drawing branches, with angle determined by the user mouse position.'
   );
+  
 }
 
 function branch(h, level) {
@@ -81,50 +108,49 @@ function branch(h, level) {
     translate(0, -h);
     branch(h, level + 1);
     pop();
+
   }
 }
 
+
 // _________________________________
 
-// Define graphic as a global variable. This variable
-// will be the offscreen buffer.
-let graphic;
+// let graphic;
 
-function setup() {
-  describe(
-    'Black canvas with a very dark grey rectangle in the middle. When the cursor is hovered over the canvas, a white circle follows the cursor in the black areas of the canvas, but not over the dark grey rectangle.'
-  );
-  createCanvas(720, 400);
+// function setup() {
+//   describe(
+//     'Black canvas with a very dark grey rectangle in the middle. When the cursor is hovered over the canvas, a white circle follows the cursor in the black areas of the canvas, but not over the dark grey rectangle.'
+//   );
+//   createCanvas(720, 400);
 
-  // Create the graphic that will be placed within the canvas.
-  graphic = createGraphics( 40, 40);
+//   // Create the graphic that will be placed within the canvas.
+//   graphic = createGraphics( 40, 40);
+// }
+
+// function draw() {
+//   // Create a black rectangle to cover the canvas.
+//   // Make the rectangle black with an alpha value of 12 so that
+//   // the white circle following the cursor slowly fades into the background.
+//   background(70, 19);
+//   // 12
+
+//   // Create the circle that will follow the cursor as it hovers
+//   // over the canvas.
+//   fill(300);
+//   noStroke();
+//   ellipse(mouseX, mouseY, 20, 20);
+
+//   // Give the buffer a dark grey background.
+//   // Any shapes within the buffer will have no fill.
+//   graphic.background(51);
+//   graphic.noFill();
+
+//   // When the cursor hovers over the offscreen buffer, replicate the
+//   // circle that is drawn when the cursor is hovering over the
+//   // canvas. Within the buffer area, only show the outline of the circle.
+//   graphic.stroke(250);
+//   graphic.ellipse(mouseX - 150, mouseY - 75, 60, 60);
+
+//   // Draw the buffer to the screen with image().
+//   image(graphic, 150, 75);
 }
-
-function draw() {
-  // Create a black rectangle to cover the canvas.
-  // Make the rectangle black with an alpha value of 12 so that
-  // the white circle following the cursor slowly fades into the background.
-  background(70, 19);
-  // 12
-
-  // Create the circle that will follow the cursor as it hovers
-  // over the canvas.
-  fill(300);
-  noStroke();
-  ellipse(mouseX, mouseY, 20, 20);
-
-  // Give the buffer a dark grey background.
-  // Any shapes within the buffer will have no fill.
-  graphic.background(51);
-  graphic.noFill();
-
-  // When the cursor hovers over the offscreen buffer, replicate the
-  // circle that is drawn when the cursor is hovering over the
-  // canvas. Within the buffer area, only show the outline of the circle.
-  graphic.stroke(250);
-  graphic.ellipse(mouseX - 150, mouseY - 75, 60, 60);
-
-  // Draw the buffer to the screen with image().
-  image(graphic, 150, 75);
-}
-
