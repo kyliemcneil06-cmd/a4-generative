f67df6c6b889eab51dd999625e7a1fd2d1f0530c
kyliemcneil06-cmd <kyliemcneil06@gmail.com>
2026-06-04 14:14:38 -0400

    messed around with sketch2

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -23,20 +23,21 @@ function setup() {
   createCanvas(720, 400);
 
   // Create the graphic that will be placed within the canvas.
-  graphic = createGraphics(405, 250);
+  graphic = createGraphics( 40, 40);
 }
 
 function draw() {
   // Create a black rectangle to cover the canvas.
   // Make the rectangle black with an alpha value of 12 so that
   // the white circle following the cursor slowly fades into the background.
-  background(0, 12);
+  background(70, 19);
+  // 12
 
   // Create the circle that will follow the cursor as it hovers
   // over the canvas.
-  fill(255);
+  fill(300);
   noStroke();
-  ellipse(mouseX, mouseY, 60, 60);
+  ellipse(mouseX, mouseY, 20, 20);
 
   // Give the buffer a dark grey background.
   // Any shapes within the buffer will have no fill.
@@ -46,7 +47,7 @@ function draw() {
   // When the cursor hovers over the offscreen buffer, replicate the
   // circle that is drawn when the cursor is hovering over the
   // canvas. Within the buffer area, only show the outline of the circle.
-  graphic.stroke(255);
+  graphic.stroke(250);
   graphic.ellipse(mouseX - 150, mouseY - 75, 60, 60);
 
   // Draw the buffer to the screen with image().
